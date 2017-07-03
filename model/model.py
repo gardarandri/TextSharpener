7032de61db01c68a1657789e98feb77cd813fdba
Garðar Andri <gardarsig95@gmail.com>
2017-07-03 20:32:55 +0000

    Generator generates large images and the sharpener can read them

--- model/model.py
+++ model/model.py
@@ -10,7 +10,8 @@ import matplotlib.image as mpimg
 
 class ImageSharpener:
     def __init__(self):
-        self.W = []
+        self.W = {}
+        self.W_cnt = 0
         self.b = []
         self.conv_info = []
 
@@ -23,57 +24,70 @@ class ImageSharpener:
         self.net_image = tf.placeholder(tf.float32, shape = [None,100,100,3])
         self.net_label = tf.placeholder(tf.float32, shape = [None,100,100,3])
 
-        self.sharpened_image = self.init_net(self.net_image)# + self.net_image
+        self.large_image = tf.placeholder(tf.float32, shape = [None,1000,1000,3])
+
+        self.sharpened_image = self.init_net(self.net_image)
 
         self.stop_criterion = "file"
+        self.save = True
 
 
 
     def leaky_relu(self, x):
         return tf.maximum(0.1*x,x)
 
-    def init_net(self, input_tensor, batch_size = None):
+    def init_net(self, input_tensor, batch_size = None, im_width=100, im_height=100):
         layer = input_tensor
 
         if batch_size == None:
             batch_size = self.batch_size
 
-        layer = tf.reshape(layer, [batch_size,100,100,3])
+        layer = tf.reshape(layer, [batch_size,im_width,im_height,3])
         
-        layer = self.add_conv_layer(layer, [5,5,3,16], 2, batch_size=batch_size)
-        layer = self.add_conv_layer(layer, [3,3,16,32], 2, batch_size=batch_size)
-        layer = self.add_conv_layer(layer, [1,1,32,32], 1, batch_size=batch_size)
-        layer = self.add_conv_layer(layer, [3,3,32,32], 1, batch_size=batch_size)
-        layer = self.add_conv_layer(layer, [3,3,32,32], 1, batch_size=batch_size)
+        layer = self.add_conv_layer(layer, [5,5,3,16], 2, batch_size=batch_size, wkey="L1")
+        layer = self.add_conv_layer(layer, [3,3,16,32], 2, batch_size=batch_size, wkey="L2")
+        layer = self.add_conv_layer(layer, [3,3,32,32], 1, batch_size=batch_size, wkey="L3")
+        layer = self.add_conv_layer(layer, [3,3,32,32], 1, batch_size=batch_size, wkey="L4")
+        layer = self.add_conv_layer(layer, [3,3,32,32], 1, batch_size=batch_size, wkey="L5")
 
-        layer = self.pop_conv_layer(layer, batch_size=batch_size)
-        layer = self.pop_conv_layer(layer, batch_size=batch_size)
-        layer = self.pop_conv_layer(layer, batch_size=batch_size)
-        layer = self.pop_conv_layer(layer, batch_size=batch_size)
-        layer = self.pop_conv_layer(layer, batch_size=batch_size)
+        layer = self.pop_conv_layer(layer, batch_size=batch_size, wkey="D1")
+        layer = self.pop_conv_layer(layer, batch_size=batch_size, wkey="D2")
+        layer = self.pop_conv_layer(layer, batch_size=batch_size, wkey="D3")
+        layer = self.pop_conv_layer(layer, batch_size=batch_size, wkey="D4")
+        layer = self.pop_conv_layer(layer, batch_size=batch_size, wkey="D5")
 
-        layer = self.conv_layer_and_weights(layer, [3,3,3,3], 1, "SAME", tf.nn.relu)
-
+        layer = self.conv_layer_and_weights(layer, [3,3,3,3], 1, "SAME", tf.nn.relu, wkey="K1")
 
         return layer
 
-    def add_conv_layer(self, x, filter_shape, stride, activation=None, batch_size=None):
+    def add_variable(self, V, wkey=None):
+        W = None
+        if wkey in self.W:
+            W = self.W[wkey]
+        else:
+            W = tf.Variable(V)
+            if wkey==None:
+                wkey = str(self.W_cnt)
+                self.W_cnt += 1
+            self.W[wkey] = W
+        return W
+
+    def add_conv_layer(self, x, filter_shape, stride, activation=None, batch_size=None, wkey=None):
         if activation == None:
             activation = self.leaky_relu
         if batch_size == None:
             batch_size = self.batch_size
 
         input_size = x.get_shape().as_list()
-        W = tf.Variable(tf.random_normal(filter_shape, stddev=self.winit / math.sqrt(reduce(lambda x,y: x*y,filter_shape,1.0))))
+
+        W = self.add_variable(tf.random_normal(filter_shape, stddev=self.winit / math.sqrt(reduce(lambda x,y: x*y,filter_shape,1.0))), wkey)
         x = tf.nn.conv2d(x, W, [1,stride,stride,1], padding="SAME")
 
         self.conv_info.append((W,input_size,stride))
 
-        self.W.append(W)
-
         return activation(x)
 
-    def pop_conv_layer(self, x, activation=None, batch_size=None):
+    def pop_conv_layer(self, x, activation=None, batch_size=None, wkey=None):
         if activation == None:
             activation = self.leaky_relu
         if batch_size == None:
@@ -81,37 +95,37 @@ class ImageSharpener:
 
         info = self.conv_info.pop()
 
-        print(info[1])
-        W = tf.Variable(tf.random_normal(info[0].get_shape().as_list(), stddev=self.winit / math.sqrt(reduce(lambda x,y: x*y,info[0].get_shape().as_list(),1.0))))
+        W = self.add_variable(tf.random_normal(info[0].get_shape().as_list(), stddev=self.winit / math.sqrt(reduce(lambda x,y: x*y,info[0].get_shape().as_list(),1.0))), wkey)
         x = tf.nn.conv2d_transpose(x, W, tf.stack(info[1]), [1,info[2],info[2],1], padding="SAME")
-
-        self.W.append(W)
 
         return activation(x)
 
-    def res_layer(self, x, filter_size):
-        tmp = self.conv_layer_and_weights(x, [filter_size, filter_size, self.m_channels, self.m_channels], 1, "SAME", tf.nn.relu)
-        return tmp + x
-
-    def conv_layer_and_weights(self, x, conv_dims, stride, padding, activation):
+    def conv_layer_and_weights(self, x, conv_dims, stride, padding, activation, wkey=None):
         #mean, variance = tf.nn.moments(x, [0])
         #x = tf.nn.batch_normalization(x, mean, variance, tf.Variable(tf.random_normal(mean.get_shape().as_list())), tf.Variable(tf.random_normal(mean.get_shape().as_list())), 0.01)
 
-        W = tf.Variable(tf.random_normal(conv_dims,stddev=1.0/(math.sqrt(sum(conv_dims)))))
-        b = tf.Variable(tf.random_normal([1],stddev=1.0/(math.sqrt(sum(conv_dims)))))
-        self.W.append(W)
-        self.b.append(b)
+        W = self.add_variable(tf.random_normal(conv_dims,stddev=1.0/(math.sqrt(sum(conv_dims)))), wkey)
+        b = None
+        if wkey != None:
+            b = self.add_variable(tf.random_normal([1],stddev=1.0/(math.sqrt(sum(conv_dims)))), wkey+"_bias")
+        else:
+            b = self.add_variable(tf.random_normal([1],stddev=1.0/(math.sqrt(sum(conv_dims)))))
 
         return activation(tf.add(tf.nn.conv2d(x, W, [1,stride,stride,1], padding=padding), b))
 
-    def conv_layer(self,x,W,b,activation):
-        res = tf.add(tf.nn.conv2d(x,W,[1,1,1,1],padding="SAME"),b)
-        return activation(res)
+    #def res_layer(self, x, filter_size):
+    #    tmp = self.conv_layer_and_weights(x, [filter_size, filter_size, self.m_channels, self.m_channels], 1, "SAME", tf.nn.relu)
+    #    return tmp + x
 
-    def de_conv_layer(self, x,W,b,activation, targ_shape):
-        return self.conv_layer(tf.image.resize_images(x,targ_shape),W,b,activation)
 
-    def make_file_pipeline(self, image_files, label_files, batch_size = None):
+    #def conv_layer(self,x,W,b,activation):
+    #    res = tf.add(tf.nn.conv2d(x,W,[1,1,1,1],padding="SAME"),b)
+    #    return activation(res)
+
+    #def de_conv_layer(self, x,W,b,activation, targ_shape):
+    #    return self.conv_layer(tf.image.resize_images(x,targ_shape),W,b,activation)
+
+    def make_file_pipeline(self, image_files, label_files, batch_size = None, im_width=100, im_height=100):
         if batch_size == None:
             batch_size = self.batch_size
 
@@ -126,8 +140,8 @@ class ImageSharpener:
         image = tf.to_float(tf.image.decode_png(image, channels = 3)) / 256.0
         label = tf.to_float(tf.image.decode_png(label, channels = 3)) / 256.0
 
-        image = tf.reshape(image,[100,100,3])
-        label = tf.reshape(label,[100,100,3])
+        image = tf.reshape(image,[im_width,im_height,3])
+        label = tf.reshape(label,[im_width,im_height,3])
 
         image_batch, label_batch = tf.train.batch([image,label], batch_size = batch_size, capacity = 1000)
 
@@ -147,6 +161,10 @@ class ImageSharpener:
             })[i])
         plt.savefig("iter"+str(iteration)+".png")
 
+    def start_session(self):
+        sess = tf.Session()
+        return sess
+
     def train_on_images(self, train_files, label_files, val_train_files, val_label_files):
         image, label = self.make_file_pipeline(train_files, label_files)
         val_image, val_label = self.make_file_pipeline(val_train_files, val_label_files)
@@ -164,7 +182,7 @@ class ImageSharpener:
 
         init_op  = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
 
-        sess = tf.Session()
+        sess = self.start_session()
         writer = tf.summary.FileWriter("../logs/", sess.graph)
         sess.run(init_op)
 
@@ -183,6 +201,7 @@ class ImageSharpener:
 
             if self.stop_criterion == "file":
                 try:
+                    # If stopfile exists then we stop training
                     open("stopfile")
                     break
                 except IOError:
@@ -191,20 +210,12 @@ class ImageSharpener:
 
             im_lab = sess.run([image,label])
 
-            print(self.conv_info)
-            a = sess.run(self.W[-1])
+            a = sess.run(self.W["L1"])
             print(a[0,0,0,0])
             sess.run(train, feed_dict = {
                 self.net_image : im_lab[0],
                 self.net_label : im_lab[1]
                 })
-            #b = sess.run(self.W)
-            #prstr = ""
-            #for i in range(len(self.W)):
-            #    prstr = prstr + " " + str(i) + ": "
-            #    prstr = prstr + str(np.linalg.norm(a[i] - b[i]))
-
-            #print(prstr)
 
             if iteration % 5 == 0:
                 val_im_lab = sess.run([val_image, val_label])
@@ -238,16 +249,31 @@ class ImageSharpener:
         plt.plot(tr_iter, vl_cost)
         plt.savefig("train_plot.png")
 
+        if self.save == True:
+            saver = tf.train.Saver()
+            saver.save(sess,"../savedmodels/sharpener")
 
-    def sharpen(self, train_files, name_suffix=""):
+        return sess
+
+    def load_model(self, model_path):
+        sess = tf.Session()
+        saver = tf.train.Saver()
+        saver.restore(sess, model_path)
+
+        return sess
+
+    def sharpen(self, train_files, name_suffix="", sess = None):
+        self.sharpened_image = self.init_net(self.net_image,1)
+
         image, label = self.make_file_pipeline(train_files, train_files,1)
 
         image_out = tf.image.encode_png(tf.reshape(tf.cast((self.sharpened_image)*256.0,tf.uint8),[100,100,3]))
 
         init_op  = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
 
-        sess = tf.Session()
-        sess.run(init_op)
+        if sess == None:
+            sess = tf.Session()
+            sess.run(init_op)
 
         coord = tf.train.Coordinator()
         threads = tf.train.start_queue_runners(sess=sess, coord=coord)
@@ -261,8 +287,34 @@ class ImageSharpener:
 
         coord.request_stop()
         coord.join(threads)
-            
 
+    def test(self, sess):
+        train_files = ["../data/large_train"+str(i)+".png" for i in range(10)]
+
+        self.sharpened_image = self.init_net(self.large_image,1,1000,1000)
+
+        image, label = self.make_file_pipeline(train_files,train_files,1,im_width=1000,im_height=1000)
+
+        image_out = tf.image.encode_png(tf.reshape(tf.cast((self.sharpened_image)*256.0,tf.uint8),[1000,1000,3]))
+
+        init_op  = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
+
+        if sess == None:
+            sess = tf.Session()
+            sess.run(init_op)
+
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+        for f_str in train_files:
+            out_file = open(f_str[:-4]+"_sharpened.png","w")
+            out_file.write(sess.run(image_out, feed_dict = {
+                self.large_image : sess.run(image)
+                }))
+            out_file.close()
+
+        coord.request_stop()
+        coord.join(threads)
 
 
 
@@ -270,21 +322,20 @@ ims = ImageSharpener()
 
 tf.set_random_seed(5)
 
-#ims.test()
-
-#ims.sharpen(
-#        ["../data/validation_1_train"+str(i)+".png" for i in range(10)], "_before"
+#sess = ims.train_on_images(
+#        ["../data/set_1_train"+str(i)+".png" for i in range(1000)],
+#        ["../data/set_1_label"+str(i)+".png" for i in range(1000)],
+#        ["../data/validation_1_train"+str(i)+".png" for i in range(10)],
+#        ["../data/validation_1_label"+str(i)+".png" for i in range(10)]
 #        )
 
-ims.train_on_images(
-        ["../data/set_1_train"+str(i)+".png" for i in range(1000)],
-        ["../data/set_1_label"+str(i)+".png" for i in range(1000)],
-        ["../data/validation_1_train"+str(i)+".png" for i in range(10)],
-        ["../data/validation_1_label"+str(i)+".png" for i in range(10)]
-        )
+sess = ims.load_model("../savedmodels/sharpener")
 
 #ims.sharpen(
-#        ["../data/validation_1_train"+str(i)+".png" for i in range(10)], "_after"
+#        ["../data/validation_1_train"+str(i)+".png" for i in range(10)], "_after",
+#        sess
 #        )
 
+
+ims.test(sess)
 
