f52668d5a450899ab19e33f280f3825cb2941acb
Garðar Andri <gardarsig95@gmail.com>
2017-07-09 20:06:02 +0000

    Setup for training

--- Report/netimage.py
+++ Report/netimage.py
@@ -1,32 +1,49 @@
 
 
 
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
+from PIL import Image, ImageDraw
 
 
 
+im = Image.new("RGB", (1500,400), (255,255,255))
+imd = ImageDraw.Draw(im)
 
-fig = plt.figure(figsize=(10,10))
-ax = fig.add_subplot(111)
-ax.axis("off")
-
-def add_layer(inax, num_channels, x, y, d):
+def add_layer(num_channels, x, y, d, w, s):
+    ox = x
+    oy = y
     for i in range(num_channels):
-        inax.add_patch(
-                patches.Rectangle(
-                    (x + d*i,y-d*i),
-                    0.1,
-                    0.1
-                    )
-                )
+        x = x + 0.7*d
+        y = y + d
+        imd.rectangle((x,y,x+w,y+w), outline=(0,0,0), fill=(170,170,170))
 
-add_layer(ax, 3, 0.1, 0.6, 0.02)
-add_layer(ax, 32, 0.3, 0.6, 0.004)
-add_layer(ax, 32, 0.5, 0.6, 0.004)
-add_layer(ax, 32, 0.7, 0.6, 0.004)
+    imd.text((x,y+w+0.5*w),"{} channels".format(num_channels), fill=(0,0,0))
+    imd.text((ox+num_channels*d/2+1.1*w,oy+num_channels*d/2),"{}".format(s), fill=(0,0,0))
 
-fig.savefig("test.png", bbox_inches="tight")
+
+nc = [3,16,32,32,32,32,32,32,16,3]
+atx = 10
+wd = [70] + [50] + [25]*6 + [50] + [70]
+dif = [20] + [10] + [5]*6 + [10] + [20]
+aty = [90]+[40]*9
+offset = [50] + [0]*9
+apstr = ["l_relu"]*8+["relu"]+[""]
+for i in range(len(nc)):
+    add_layer(nc[i], atx, aty[i], dif[i], wd[i], apstr[i])
+    atx += 0.7*dif[i]*nc[i] + wd[i] + offset[i]
+
+#add_layer(ax, 3,  0.04, 0.4, 0.02, 0.1)
+#add_layer(ax, 16, 0.04, 0.4, 0.008, 0.1)
+#add_layer(ax, 32, 0.04, 0.4, 0.004, 0.1)
+#add_layer(ax, 32, 0.04, 0.4, 0.004, 0.1)
+#add_layer(ax, 32, 0.04, 0.4, 0.004, 0.1)
+#
+#add_layer(ax, 32, 0.04, 0.4, 0.004, 0.1)
+#add_layer(ax, 32, 0.04, 0.4, 0.004, 0.1)
+#add_layer(ax, 32, 0.04, 0.4, 0.004, 0.1)
+#add_layer(ax, 16, 0.04, 0.4, 0.008, 0.1)
+#add_layer(ax, 3,  0.04, 0.4, 0.02, 0.1)
+
+im.save("architecture.png")
 
 
 
